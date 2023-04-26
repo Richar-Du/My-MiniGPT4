c5ebd8865d3bda9a5ac8f44caede8056dc9f56e0
duyifan04 <duyifan04@meituan.com>
2023-04-26 23:18:31 +0800

    add vqa for stage2

---
+++ minigpt4/datasets/builders/vqa_builder.py
@@ -0,0 +1,21 @@
+"""
+ Copyright (c) 2022, salesforce.com, inc.
+ All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+ For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+"""
+
+from minigpt4.datasets.builders.base_dataset_builder import BaseDatasetBuilder
+
+from minigpt4.common.registry import registry
+from minigpt4.datasets.datasets.coco_vqa_datasets import COCOVQADataset, COCOVQAEvalDataset
+
+@registry.register_builder("coco_vqa")
+class COCOVQABuilder(BaseDatasetBuilder):
+    train_dataset_cls = COCOVQADataset
+    eval_dataset_cls = COCOVQAEvalDataset
+
+    DATASET_CONFIG_DICT = {
+        "default": "configs/datasets/coco/defaults_vqa.yaml",
+        "eval": "configs/datasets/coco/eval_vqa.yaml",
+    }
