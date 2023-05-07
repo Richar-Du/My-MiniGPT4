d74d21dad46fd80c62f0924c543b990cd6afa591
Richar-Du <wfkwdyf@163.com>
2023-05-07 09:20:27 +0800

    unfreeze raise unscale fp16 error, comment half()

--- minigpt4/models/eva_vit.py
+++ minigpt4/models/eva_vit.py
@@ -409,7 +409,7 @@ def convert_weights_to_fp16(model: nn.Module):
 #                 if tensor is not None:
 #                     tensor.data = tensor.data.half()
 
-    model.apply(_convert_weights_to_fp16)
+    # model.apply(_convert_weights_to_fp16)
     
     
 def create_eva_vit_g(img_size=224,drop_path_rate=0.4,use_checkpoint=False,precision="fp16"):
@@ -436,7 +436,7 @@ def create_eva_vit_g(img_size=224,drop_path_rate=0.4,use_checkpoint=False,precis
     incompatible_keys = model.load_state_dict(state_dict, strict=False)
 #     print(incompatible_keys)
     
-    if precision == "fp16":
-#         model.to("cuda") 
-        convert_weights_to_fp16(model)
+#     if precision == "fp16":
+# #         model.to("cuda") 
+#         convert_weights_to_fp16(model)
     return model
